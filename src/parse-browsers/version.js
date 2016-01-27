cf6780b40ce97b65084416f9e59246de109d6f09
Eryk Napierała <eryk.piast@gmail.com>
2016-01-27 21:24:29 +0000

    remove unused file

--- src/parse-browsers/version.js
+++
@@ -1,8 +0,0 @@
-// export function adjustTrailingZeros(version, fields = 3) {
-//   const splitted = version.split('.').reverse();
-//   for (let i; i < fields; i++) {
-//     if (splitted[i] === '0') {
-//       splitted = [i]
-//     }
-//   }
-// }
